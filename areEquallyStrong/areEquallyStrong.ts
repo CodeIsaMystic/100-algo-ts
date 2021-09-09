a8ab72eff87ea56633e5199d6ace980cf4aca36f
Julien Orjollet <oj.professionnel@gmail.com>
2021-09-09 15:48:26 +0200

    code for alternativeSums & areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,19 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight
+  const friendsStrongest =
+    friendsLeft >= friendsRight ? friendsLeft : friendsRight
 
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
