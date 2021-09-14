b1e40f0e4baa1b688caa6c0e24097766bc2916f5
Julien Orjollet <oj.professionnel@gmail.com>
2021-09-14 17:13:44 +0200

    resolve insensitive palindrome algo

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,13 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+  const originalLowerCase = inputString.toLowerCase()
+  const reversedWord = originalLowerCase
+    .toUpperCase()
+    .split("")
+    .reverse()
+    .join("")
 
+  return originalLowerCase === reversedWord
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"))
+console.log(isCaseInsensitivePalindrome("abac"))
