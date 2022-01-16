80b01602756d99f692e1e3be08973f5903a767a6
Julien Orjollet <oj.professionnel@gmail.com>
2022-01-16 19:21:50 +0100

    resolving century from year algo

--- centuryFromYear/centuryFromYear.spec.ts
+++ centuryFromYear/centuryFromYear.spec.ts
@@ -1,25 +1,25 @@
-import { centuryFromYear } from './centuryFromYear';
+import { centuryFromYear } from "./centuryFromYear"
 
-xdescribe(centuryFromYear.name, () => {
-    it('Test 1', () => {
-        // arrange
-        const data = 1905;
+describe(centuryFromYear.name, () => {
+  it("Test 1", () => {
+    // arrange
+    const data = 1905
 
-        // act
-        const response = centuryFromYear(data);
+    // act
+    const response = centuryFromYear(data)
 
-        // assert
-        expect(response).toBe(20);
-    });
+    // assert
+    expect(response).toBe(20)
+  })
 
-    it('Test 2', () => {
-        // arrange
-        const data = 1700;
+  it("Test 2", () => {
+    // arrange
+    const data = 1700
 
-        // act
-        const response = centuryFromYear(data);
+    // act
+    const response = centuryFromYear(data)
 
-        // assert
-        expect(response).toBe(17);
-    });
-});
+    // assert
+    expect(response).toBe(17)
+  })
+})
--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,12 @@
 export function centuryFromYear(year: number): number {
+  const century = year / 100
 
+  if (year % 100 === 0) {
+    return century
+  }
+
+  return Math.floor(century) + 1
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1905))
+console.log(centuryFromYear(1700))
