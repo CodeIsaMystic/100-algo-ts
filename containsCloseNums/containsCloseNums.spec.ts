1ae7136d08ea6e0a2682baac431258d185650c5c
Julien Orjollet <oj.professionnel@gmail.com>
2022-01-22 09:21:44 +0100

    resolved containsCloseNums algo

--- containsCloseNums/containsCloseNums.spec.ts
+++ containsCloseNums/containsCloseNums.spec.ts
@@ -1,27 +1,27 @@
-import { containsCloseNums } from './containsCloseNums';
+import { containsCloseNums } from "./containsCloseNums"
 
 xdescribe(containsCloseNums.name, () => {
-    it('Test 1', () => {
-        // arrange
-        const nums = [0, 1, 2, 3, 5, 2];
-        const k = 3;
+  it("Test 1", () => {
+    // arrange
+    const nums = [0, 1, 2, 3, 5, 2]
+    const k = 3
 
-        // act
-        const response = containsCloseNums(nums, k);
+    // act
+    const response = containsCloseNums(nums, k)
 
-        // assert 
-        expect(response).toBe(true);
-    });
+    // assert
+    expect(response).toBe(true)
+  })
 
-    it('Test 2', () => {
-        // arrange
-        const nums = [0, 1, 2, 3, 5, 2];
-        const k = 2;
+  it("Test 2", () => {
+    // arrange
+    const nums = [0, 1, 2, 3, 5, 2]
+    const k = 2
 
-        // act
-        const response = containsCloseNums(nums, k);
+    // act
+    const response = containsCloseNums(nums, k)
 
-        // assert 
-        expect(response).toBe(false);
-    });
-});
+    // assert
+    expect(response).toBe(false)
+  })
+})
