d3cfa5507577ffac89cdf8ff8422177c7c85c4fc
Julien Orjollet <oj.professionnel@gmail.com>
2022-01-24 21:35:15 +0100

    add depositProfit algo

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,17 @@
-export function depositProfit(deposit: number, rate: number, threshold: number): number {
+export function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let year = 0
+  let account = deposit
 
+  while (threshold > account) {
+    account += account * (rate / 100)
+    year++
+  }
+
+  return year
 }
 
 // console.log(depositProfit(100, 20, 170))
